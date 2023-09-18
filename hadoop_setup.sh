bf0adc97f56ccabc614062412f23ac4e059e666d
sifisKoen <sifis.k.i@gmail.com>
2023-09-18 12:21:59 +0200

    Re-configure hadoop setup

--- hadoop_setup.sh
+++ hadoop_setup.sh
@@ -1,61 +1,75 @@
 #!/bin/sh
 
-# Create a function to set a new password for our new user
-set_user_password(){
-    user_name="$1"
-
-    while true ; do
-        
-        printf "Please enter a passowrd for %s user: " "$user_name"
-        read -r new_user_password
-        echo
-
-        printf "Please re-enter the password for %s user. For confirmation: " "$user_name"
-        read -r confirm_password
-        echo
-
-        if [ "$new_user_password" = "$confirm_password" ]; then
-            echo "$user_name:$new_user_password" | sudo chpasswd
-            break
-        else
-            echo "Password do not match. Please try again."
-        fi
-        
-    done
-
-}
-
-# Create a function to generate SSH key for the user
-generate_new_ssh_key(){
-
-    user_name="$1"
-
-    # Generate SSH key and add to authorized_keys
-    sudo -u hadoop sh -c '
-    ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
-    cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-    '
-
-}
-
-# Create a function to install Java
+# Install Java
 install_java(){
 
+    echo "Installing Java..."    
     sudo apt update
-    sudo apt intall default-jdk default-jre -y
-    sudo hava -version
+    sudo apt install default-jdk default-jre -y
+    sudo java -version
+
+}
+
+create_hadoop_user(){
+    
+    printf "Do you want to create new user to install Hadoop? (yes/no): " >&2
+    read -r choice
+
+    if [ "$choice" = "yes" ] || [ "$choice" = "y" ]; then
+
+        printf "Please enter the new username: " >&2
+        read -r new_user_name
+        user_home="/opt/$new_user_name"
+        echo "Creating hadoop user..." >&2
+        sudo useradd -r "$new_user_name" -m -d "$user_home" --shell /bin/bash
+
+        # Set a new password
+        echo "Add password for hadoop user" >&2
+        sudo passwd "$new_user_name"
+    else
+        new_user_name=$(whoami)
+        user_home="$HOME"
+    fi
+
+    echo "$new_user_name $user_home"
+        
+}
+
+generate_new_ssh_key(){
+
+    user="$1"
+    home_dir="$2"
+    
+    # Generate ssh keys for hadoop user and set up local ssh
+    echo "Setting up SSH for $user..."
+    sudo -u "$user" ssh-keygen -t rsa -f "$home_dir/.ssh/id_rsa" -N ""
+    sudo -u "$user" sh -c "cat $home_dir/.ssh/id_rsa.pub > $home_dir/.ssh/authorized_keys"
+    # sudo -u hadoop ssh-keyscan localhost >> /opt/hadoop/.ssh/known_hosts
+
 }
 
 
-install_hadoop(){
+downlaod_and_extract_hadoop(){
+    
+    user="$1"
+    home_dir="$2"
 
-    user_name="$1"
+    # Download and extract Hadoop for hadoop user
+    echo "Downloading and extracting Hadoop..."
+    sudo -u "$user" wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.4/hadoop-3.2.4.tar.gz -O "$home_dir/hadoop-3.2.4.tar.gz"
+    sudo -u "$user" tar -xzvf "$home_dir/hadoop-3.2.4.tar.gz" -C "$home_dir" --strip-components=1
 
-    sudo -u "$user_name" wget https://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-3.2.4/hadoop-3.2.4.tar.gz -O hadoop-3.2.4.tar.gz
+}
 
-    sudo -u "$user_name" sh -c 'cat >> ~/.bashrc' <<EOL
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export HADOOP_HOME=/opt/hadoop
+configure_hadoop_environment(){
+
+    user="$1"
+    home_dir="$2"
+
+    # Add environment variables to hadoop's bashrc
+sudo -u "$user" bash -c "cat >> $home_dir/.bashrc" <<EOL
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export HADOOP_HOME=$home_dir
 export PATH=\$PATH:\$HADOOP_HOME/bin
 export PATH=\$PATH:\$HADOOP_HOME/sbin
 export HADOOP_MAPRED_HOME=\$HADOOP_HOME
@@ -65,35 +79,20 @@ export YARN_HOME=\$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=\$HADOOP_HOME/lib/native
 export HADOOP_OPTS="-Djava.library.path=\$HADOOP_HOME/lib"
 EOL
+
 }
 
+main(){
 
-printf "Do you want to create a user for Hadoop? (yea/no): "
-read -r user_response
-
-if [ "$user_response" = "yes" ] || [ "$user_response" = "y" ]; then
-    printf "Enter the name for the Hadoop user: "
-    read -r hadoop_user_name
-
-    sudo useradd -r "$hadoop_user_name" -m -b /opt/hadoop --shell /bim/bash
-
-    set_user_password "$hadoop_user_name"
-    generate_new_ssh_key "$hadoop_user_name"
-    install_java
-    install_hadoop "$hadoop_user_name"
-
-else
-    printf "Do you want to install Hadoop on your current user? (yes/no): "
-    read -r user_response_2
-
-    if [ "$user_response_2" = "yes" ] || [ "$user_response_2" = "y" ]; then
-        current_user=$(whoami)
-        generate_new_ssh_key "$current_user"
+    create_hadoop_user | {
+        read -r user user_home
+        generate_new_ssh_key "$user" "$user_home"
         install_java
-        install_hadoop "$current_user"
-    else
-        echo "Exiting ..."
-        exit 0
-    fi
-fi
+        downlaod_and_extract_hadoop "$user" "$user_home"
+        configure_hadoop_environment "$user" "$user_home"
+        echo "Hadoop setup complete for $user !"
+    }    
+    
+}
 
+main
